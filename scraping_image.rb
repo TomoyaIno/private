8a359c0a94334339a278953298b0b242b0dddded
EC2 Default User <ec2-user@ip-172-31-33-166.us-east-2.compute.internal>
2019-08-29 12:45:02 +0000

    トップページ作成

--- scraping_image.rb
+++ scraping_image.rb
@@ -1,8 +1,20 @@
  require 'mechanize'
  
- agent = Mechanize.new
- page = agent.get("https://hikyou.jp/report/dayuse/67724/")
- elements = page.search(".size-full.wp-image-67739")
- elements.each do |ele|
-  puts ele.get_attribute('src')
+ def scraping_image(link)
+  agent = Mechanize.new
+  page = agent.get(link)
+  image_url = page.at('.size-full.wp-image-67739').get_attribute('src')
+  return image_url
  end
+ 
+ links = []
+ agent = Mechanize.new
+ current_page = agent.get("https://hikyou.jp")
+ elements = current_page.search('.wrap.blm h1')
+ elements.each do |ele|
+  links << ele.get_attribute('href')
+ end
+ 
+ links.each do |link|
+  puts scraping_image('https://hikyou.jp' + link)
+end
--- scraping_title.rb
+++ scraping_title.rb
@@ -2,7 +2,7 @@ require 'mechanize'
 
 agent = Mechanize.new
 page = agent.get("https://hikyou.jp")
-elements = page.search("h1")
+elements = page.search(".wrap.blm h1")
 elements.each do |ele|
   puts ele.inner_text
 end
