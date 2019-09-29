e82cb0e011be96f5eb605e0972819a69ad8cb37e
EC2 Default User <ec2-user@ip-172-31-33-166.us-east-2.compute.internal>
2019-09-29 07:36:01 +0000

    詳細画面の口コミでニックネームと本文をデータベースから持ってくることに成功

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,5 +6,12 @@ class ReviewsController < ApplicationController
   end
   
   def create
+    Review.create(create_params)
+    redirect_to controller: :products, action: :index
+  end
+  
+  private
+  def create_params
+    params.require(:review).permit(:nickname, :rate, :review).merge(product_id: params[:product_id])
   end
 end
